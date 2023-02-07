c06995d311584af714b16385083cb8c29bc0935a
ArtitayaKhaudsri <79126353+ArtitayaKhaudsri@users.noreply.github.com>
2023-02-07 22:59:28 +0700

    use functional

--- components/molecules/search-bar.js
+++ components/molecules/search-bar.js
@@ -1,114 +1,96 @@
 import React, {useState} from 'react'
 
-class SearchBar extends React.Component {
+export default function Geography() {
 
+    const [searchInput, setSearchInput] = useState("");
+    const handleChange = (e) => {
+        e.preventDefault();
+        setSearchInput(e.target.value);
+        console.log(searchInput)
+    };
 
-    constructor(props) {
-        super(props);
+    const columns = [
+        {
+            key: "order",
+            label: "Order",
+        },
+        {
+            key: "country",
+            label: "Country",
+        },
+        {
+            key: "player",
+            label: "Player today",
+        },
+        {
+            key: "statusPlayer",
+            label: "Status player",
+        },
+    ];
+    const rows = [
+        {
+            key: "4",
+            order: "4",
+            country: "Country4",
+            player: "Player",
+            statusPlayer: "up",
+        },
+        {
+            key: "5",
+            order: "5",
+            country: "Country5",
+            player: "Player",
+            statusPlayer: "up",
+        },
+        {
+            key: "6",
+            order: "6",
+            country: "Country6",
+            player: "Player",
+            statusPlayer: "down",
+        },
+        {
+            key: "7",
+            order: "7",
+            country: "Country7",
+            player: "Player",
+            statusPlayer: "up",
+        },
+        {
+            key: "8",
+            order: "8",
+            country: "Country8",
+            player: "Player",
+            statusPlayer: "up",
+        },
+        {
+            key: "9",
+            order: "9",
+            country: "Country9",
+            player: "Player",
+            statusPlayer: "down",
+        },
+        {
+            key: "10",
+            order: "10",
+            country: "Country10",
+            player: "Player",
+            statusPlayer: "down",
+        },
+    ];
+
+    const arrowColor = (statusPlayer) => {
+        if (statusPlayer==="down") {
+            return <div style={{ width:"0px", height:"0px", borderLeft: "10px solid transparent", borderRight: "10px solid transparent", borderTop: "10px solid #F45D48"}}></div>
+        }
+        else {
+            return <div style={{ width:"0px", height:"0px", borderLeft: "10px solid transparent", borderRight: "10px solid transparent", borderBottom: "10px solid #22CC13"}}></div>
+        }
     }
 
-    render() {
-
-        const [searchInput, setSearchInput] = useState("");
-        const handleChange = (e) => {
-            e.preventDefault();
-            setSearchInput(e.target.value);
-            console.log(searchInput)
-        };
-
-        const columns = [
-            {
-                key: "order",
-                label: "Order",
-            },
-            {
-                key: "country",
-                label: "Country",
-            },
-            {
-                key: "player",
-                label: "Player today",
-            },
-            {
-                key: "statusPlayer",
-                label: "Status player",
-            },
-        ];
-        const rows = [
-            {
-                key: "4",
-                order: "4",
-                country: "Country4",
-                player: "Player",
-                statusPlayer: "up",
-            },
-            {
-                key: "5",
-                order: "5",
-                country: "Country5",
-                player: "Player",
-                statusPlayer: "up",
-            },
-            {
-                key: "6",
-                order: "6",
-                country: "Country6",
-                player: "Player",
-                statusPlayer: "down",
-            },
-            {
-                key: "7",
-                order: "7",
-                country: "Country7",
-                player: "Player",
-                statusPlayer: "up",
-            },
-            {
-                key: "8",
-                order: "8",
-                country: "Country8",
-                player: "Player",
-                statusPlayer: "up",
-            },
-            {
-                key: "9",
-                order: "9",
-                country: "Country9",
-                player: "Player",
-                statusPlayer: "down",
-            },
-            {
-                key: "10",
-                order: "10",
-                country: "Country10",
-                player: "Player",
-                statusPlayer: "down",
-            },
-        ];
-
-        const arrowColor = (statusPlayer) => {
-            if (statusPlayer === "down") {
-                return <div style={{
-                    width: "0px",
-                    height: "0px",
-                    borderLeft: "10px solid transparent",
-                    borderRight: "10px solid transparent",
-                    borderTop: "10px solid #F45D48"
-                }}></div>
-            } else {
-                return <div style={{
-                    width: "0px",
-                    height: "0px",
-                    borderLeft: "10px solid transparent",
-                    borderRight: "10px solid transparent",
-                    borderBottom: "10px solid #22CC13"
-                }}></div>
-            }
-        }
-
-        return (
-            <div style={{paddingLeft:"2%", paddingRight:"2%"}}>
-                <h1 style={{fontSize: "30px"}}>Geography Search</h1>
+    return (
+        <div style={{paddingLeft:"2%", paddingRight:"2%"}}>
+            <h1 style={{fontSize: "30px"}}>Geography Search</h1>
                 <div style={{padding:"19px 85px 0px 18px"}}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                          stroke="currentColor" className="w-6 h-6" style={{width:"63px", height:"40px", paddingLeft:"2%", paddingTop:"2px", position:"absolute"}}>
@@ -156,7 +138,7 @@ class SearchBar extends React.Component {
                 <div style={{paddingTop:"264px"}}>
                     <div style={{ height: "auto", width: "100%", backgroundColor:"#F6F6F6", borderRadius:"10px 19px 0px 0px"}}>
                         <div style={{paddingTop:"17px"}}>
-                            {this.rows.map((item) => (
+                            {rows.map((item) => (
                                 <div style={{paddingTop:"12px", paddingRight:"4%", paddingLeft:"4%"}}>
                                     <div style={{ backgroundColor:"white", borderRadius:"10px", width: "100%", display:"flex", height:"80px",paddingLeft:"3%", paddingRight:"3%"}} >
                                         <div style={{ width:"5%", margin:"auto"}}>
@@ -175,10 +157,6 @@ class SearchBar extends React.Component {
                         </div>
                     </div>
                 </div>
-            </div>
-        )
-    }
+         </div>
+    )
 }
-
-
-export default SearchBar;
