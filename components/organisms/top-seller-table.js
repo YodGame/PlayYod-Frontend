fe4b24da25c216d45934f03522cbeca35d54b302
ArtitayaKhaudsri <79126353+ArtitayaKhaudsri@users.noreply.github.com>
2023-02-13 20:36:30 +0700

    top seller table update can set graph

--- components/organisms/top-seller-table.js
+++ components/organisms/top-seller-table.js
@@ -26,49 +26,51 @@ ChartJs.register(
 )
 export default function TopSellerTable(){
 
-    const dataGraph = {
-        type:'bar',
-        labels: ['May', 'June', 'July', 'July', 'July'],
-        datasets: [
+    const graph =(dataGraph) => {
+        const graphJson =
             {
-                label: 'gameName',
-                fill: true,
-                lineTension: 0.4,
-                threshold:4,
-                includeInvisible:true,
-                autoPadding:true,
-                color: 'Black',
-                borderColor: 'Black',
-                pointBorderColor: 'Black',
-                pointBackgroundColor: '#fff',
-                pointHoverBorderWidth: 2,
-                pointRadius: 1,
-                pointHitRadius: 10,
-                data: [ 56, 55, 40, 74,60],
-            }
-        ],
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-            scales: {
-                xAxes: [{
-                    ticks: { display: false },
-                    gridLines: {
-                        display: false,
-                        drawBorder: false
-                    }
+                type:'bar',
+                labels: ['May', 'June', 'July', 'July', 'July'],
+                datasets: [
+                {
+                    label: 'gameName',
+                    fill: true,
+                    lineTension: 0.4,
+                    threshold:4,
+                    includeInvisible:true,
+                    color: 'Black',
+                    borderColor: 'Black',
+                    pointBorderColor: 'Black',
+                    pointHoverBorderWidth: 2,
+                    pointRadius: 1,
+                    pointHitRadius: 10,
+                    data: [56, 55, 40, 74, 60],
                 }],
-                yAxes: [{
-                    ticks: { display: false },
-                    gridLines: {
-                        display: false,
-                        drawBorder: false
+                options: {
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    scales: {
+                        xAxes: [{
+                            ticks: {display: false},
+                            gridLines: {
+                                display: false,
+                                drawBorder: false
+                            }
+                        }],
+                        yAxes: [{
+                            ticks: {display: false},
+                            gridLines: {
+                                display: false,
+                                drawBorder: false
+                            }
+                        }]
                     }
-                }]
+                }
             }
-        }
+        graphJson.datasets[0].data = dataGraph;
+        return graphJson
+    }
 
-    };
 
     const columns = [
         {
@@ -85,51 +87,61 @@ export default function TopSellerTable(){
             key: "1",
             GameName: "PUBG",
             Sell: "70,000,000",
+            DataGraph: [56, 55, 40, 74, 60],
         },
         {
             key: "2",
             GameName: "Lost Ark",
             Sell: "50,000,000",
+            DataGraph: [80, 55, 70, 74, 59],
         },
         {
             key: "3",
             GameName: "Counter-Strike",
             Sell: "40,000,000",
+            DataGraph: [62, 55, 40, 70, 60],
         },
         {
             key: "4",
             GameName: "Dota2",
             Sell: "1,579,631",
+            DataGraph: [56, 55, 40, 74, 60],
         },
         {
             key: "5",
             GameName: "ELDEN RING",
             Sell: "1,265,897",
+            DataGraph: [56, 55, 40, 74, 60],
         },
         {
             key: "6",
             GameName: "Game Name",
             Sell: "952,523",
+            DataGraph: [56, 55, 40, 74, 60],
         },
         {
             key: "7",
             GameName: "Game Name",
             Sell: "952,523",
+            DataGraph: [56, 55, 40, 74, 60],
         },
         {
             key: "8",
             GameName: "Game Name",
             Sell: "952,523",
+            DataGraph: [56, 55, 40, 74, 60],
         },
         {
             key: "9",
             GameName: "Game Name",
             Sell: "952,523",
+            DataGraph: [56, 55, 40, 74, 60],
         },
         {
             key: "10",
             GameName: "Game Name",
             Sell: "952,523",
+            DataGraph: [56, 55, 40, 74, 60],
         },
     ];
 
@@ -155,7 +167,7 @@ export default function TopSellerTable(){
     return (
         <div style={{paddingTop:"17px",paddingLeft:"5%", paddingRight:"5%", color:"black"}}>
             <div style={{display:"flex"}}>
-                {columns.map((item, index) => (
+                {columns.map((item) => (
                     <h5 style={{paddingLeft:"6%", width:"40%", fontSize:"25px"}}>{item.label}</h5>
                 ))}
             </div>
@@ -174,8 +186,9 @@ export default function TopSellerTable(){
                                 <h5 style={{fontSize:"35px"}} key={item.key}>{item.Sell}</h5>
                             </div>
                             <div style={{ width:"33%", margin:"auto", textAlign:"center"}}>
+
                                 <Line
-                                    data={dataGraph}
+                                    data={graph(item.DataGraph)}
                                     width={4}
                                     height={1}
                                 />
