ab62fe86f16febe4350084ab3e0bdff36a2c70f0
Naa_Fiew <fivlike@gmail.com>
2023-02-15 01:07:16 +0700

    connect API to Rating Table in Geograph

--- components/molecules/top-three-country.js
+++ components/molecules/top-three-country.js
@@ -1,25 +1,20 @@
 import React from "react";
 
-class TopThreeCountry extends React.Component {
-    constructor(props) {
-        super(props);
-    }
-    render() {
+function TopThreeCountry (props) {
 
         return (
             <div>
-                <h5 style={{backgroundColor:this.props.backgroundColor ,height:this.props.height,position:"absolute"}} className={"boxBackground"}>
-                    <img className={"img"} src={this.props.img} />
-                    <div className={this.props.style}>
-                        <h3 className={"ratingNumber"} style={{marginTop:this.props.margin}}>{this.props.rate}</h3>
-                        <h3 className={"country"}>{this.props.countryName}</h3>
+                <h5 style={{backgroundColor:props.backgroundColor ,height:props.height,position:"absolute"}} className={"boxBackground"}>
+                    <img className={"img"} src={props.img} />
+                    <div className={props.img}>
+                        <h3 className={"ratingNumber"} style={{marginTop:props.margin}}>{props.rate}</h3>
+                        <h3 className={"country"}>{props.countryName}</h3>
                     </div>
                 </h5>
         </div>
         );
     }
-}
-
+// props = backgroundColor / height / img / margin / rate / countryName
 
 
 export default TopThreeCountry;
--- components/organisms/rating_table.js
+++ components/organisms/rating_table.js
@@ -1,76 +1,5 @@
 import React from 'react'
-export default function Rating_table() {
-
-
-    const columns = [
-        {
-            key: "order",
-            label: "Order",
-        },
-        {
-            key: "country",
-            label: "Country",
-        },
-        {
-            key: "player",
-            label: "Player today",
-        },
-        {
-            key: "statusPlayer",
-            label: "Status player",
-        },
-    ];
-    const rows = [
-        {
-            key: "4",
-            order: "4",
-            country: "Country4",
-            player: "Player",
-            statusPlayer: "up",
-        },
-        {
-            key: "5",
-            order: "5",
-            country: "Country5",
-            player: "Player",
-            statusPlayer: "up",
-        },
-        {
-            key: "6",
-            order: "6",
-            country: "Country6",
-            player: "Player",
-            statusPlayer: "down",
-        },
-        {
-            key: "7",
-            order: "7",
-            country: "Country7",
-            player: "Player",
-            statusPlayer: "up",
-        },
-        {
-            key: "8",
-            order: "8",
-            country: "Country8",
-            player: "Player",
-            statusPlayer: "up",
-        },
-        {
-            key: "9",
-            order: "9",
-            country: "Country9",
-            player: "Player",
-            statusPlayer: "down",
-        },
-        {
-            key: "10",
-            order: "10",
-            country: "Country10",
-            player: "Player",
-            statusPlayer: "down",
-        },
-    ];
+export default function Rating_table(props) {
 
     const arrowColor = (statusPlayer) => {
         if (statusPlayer==="down") {
@@ -82,27 +11,32 @@ export default function Rating_table() {
     }
 
     return (
-        <div style={{paddingTop:"213px"}}>
+        <div>
             <div style={{ height: "auto", width: "100%", backgroundColor:"#F6F6F6", borderRadius:"10px 19px 0px 0px"}}>
                 <div style={{paddingTop:"17px"}}>
-                    {rows.map((item, index) => (
-                        <div style={{paddingTop:"12px", paddingRight:"4%", paddingLeft:"4%"}} key={index}>
+                    {/* {rows.map((item, index) => ( */}
+                        <div style={{paddingTop:"12px", paddingRight:"4%", paddingLeft:"4%"}} >
                             <div style={{ backgroundColor:"white", borderRadius:"10px", width: "100%", display:"flex", height:"80px",paddingLeft:"3%", paddingRight:"3%"}} >
+                                
                                 <div style={{ width:"5%", margin:"auto"}}>
-                                    <h5 style={{fontSize:"16px", color:"black"}}>{item.order}</h5>
-                                    {arrowColor(item.statusPlayer)}
+                                    <h5 style={{fontSize:"16px", color:"black"}}>{props.rank}</h5>{/* rank {item.order}*/}
+                                    {arrowColor("up")}
                                 </div>
+
                                 <div style={{ width:"65%", margin:"auto", textAlign:"center"}}>
-                                    <h5 style={{fontSize:"16px", color:"black"}}>{item.country}</h5>
+                                    <h5 style={{fontSize:"16px", color:"black"}}>{props.country}</h5>{/* Country {item.country}*/}
                                 </div>
+
                                 <div style={{ width:"30%", margin:"auto", textAlign:"center"}}>
-                                    <h5 style={{fontSize:"16px", color:"black"}}>{item.player}</h5>
+                                    <h5 style={{fontSize:"16px", color:"black"}}>{props.players}</h5>{/* PLayer {item.player}*/}
                                 </div>
+
                             </div>
                         </div>
-                    ))}
+                     {/* ))} */}
                 </div>
             </div>
         </div>
     )
+    // props = rank, country, player
 }
