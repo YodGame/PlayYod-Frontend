a53371c1dc6196bf0f4ae42bdcd4ab0c82a2a80a
ArtitayaKhaudsri <79126353+ArtitayaKhaudsri@users.noreply.github.com>
2023-03-11 20:31:54 +0700

    edit format player

--- components/organisms/rating_table.js
+++ components/organisms/rating_table.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {isNumber} from "chart.js/helpers";
 export default function Rating_table(props) {
 
     const arrowColor = (statusPlayer) => {
@@ -9,6 +10,13 @@ export default function Rating_table(props) {
             return <div style={{ width:"0px", height:"0px", borderLeft: "10px solid transparent", borderRight: "10px solid transparent", borderBottom: "10px solid #22CC13"}}></div>
         }
     }
+    const formatNumber = (number) => {
+        if (isNumber(number)) {
+            return number.toLocaleString()
+        } else {
+            return number
+        }
+    }
 
     return (
         <div>
@@ -16,7 +24,7 @@ export default function Rating_table(props) {
                 <div style={{paddingTop:"17px", paddingBottom:"17px"}}>
                     {/* {rows.map((item, index) => ( */}
                         <div style={{paddingTop:"12px", paddingRight:"4%", paddingLeft:"4%"}} >
-                            <div style={{ backgroundColor:"white", borderRadius:"10px", width: "100%", display:"flex", height:"80px",paddingLeft:"3%", paddingRight:"3%"}} >
+                            <div style={{ backgroundColor:"white", borderRadius:"10px", width: "100%", display:"flex", minHeight:"80px",paddingLeft:"3%", paddingRight:"3%", height:"auto"}} >
                                 
                                 <div style={{ width:"5%", margin:"auto"}}>
                                     <h5 style={{fontSize:"16px", color:"black"}}>{props.rank}</h5>{/* rank {item.order}*/}
@@ -28,7 +36,7 @@ export default function Rating_table(props) {
                                 </div>
 
                                 <div style={{ width:"30%", margin:"auto", textAlign:"center"}}>
-                                    <h5 style={{fontSize:"16px", color:"black"}}>{props.players}</h5>{/* PLayer {item.player}*/}
+                                    <h5 style={{fontSize:"16px", color:"black"}}>{formatNumber(props.players)}</h5>{/* PLayer {item.player}*/}
                                 </div>
 
                             </div>
