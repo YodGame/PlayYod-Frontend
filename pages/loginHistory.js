6b735da6a05b28dd46f753d377b9466c2d26cff3
NKpatimakorn <98444613+NKpatimakorn@users.noreply.github.com>
2023-03-23 00:18:14 +0700

    create loginHistory.js , topRating.js

---
+++ pages/loginHistory.js
@@ -0,0 +1,13 @@
+import Template from "@/components/layouts/template";
+import React from "react";
+
+
+export default function loginHistory() {
+    return (
+        <Template>
+            <div style={{padding:30}}>
+
+            </div>
+        </Template>
+    )
+}
---
+++ pages/topRating.js
@@ -0,0 +1,13 @@
+import Template from "@/components/layouts/template";
+import React from "react";
+
+
+export default function topRating() {
+    return (
+        <Template>
+            <div style={{padding:30}}>
+
+            </div>
+        </Template>
+    )
+}
